5e2331d96e7bebdc52bc0d83ecff1bf7cf29e876
Babichartem <Babichartemson@gmail.com>
2019-03-09 01:06:10 +0300

    GeneratedField

---
+++ RogueBearGame/Assets/Scripts/BoardGenerate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardGenerate : MonoBehaviour
+{
+    public GameObject FloorTile;
+    public GameObject WallTile;
+    private Transform boardHolder;
+
+    public int xSize { get; set; } = 10;
+    public int ySize { get; set; } = 10;
+
+    private List<Vector3> freeCells;
+
+    void Start()
+    {
+        GenerateStaticObjects();
+    }
+
+    public void GenerateStaticObjects()
+    {
+        boardHolder = new GameObject("Board").transform;
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                if (x == 0 || x == xSize-1 || y == 0 || y == ySize-1)
+                {
+                    InstantiateObject(WallTile,x,y);
+                }
+                else
+                {
+                    InstantiateObject(FloorTile,x,y);
+                }
+            }
+        }
+    }
+
+    void InstantiateObject(GameObject gameObject, int x, int y)
+    {
+        GameObject instanse = Instantiate(gameObject, new Vector3(x, y, 0.0f), Quaternion.identity);
+        instanse.transform.SetParent(boardHolder);
+    }
+
+
+    /*
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+
+}
---
+++ RogueBearGame/Assets/Scripts/Loader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject GameStarter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(GameStarter);
+    }
+  
+}
